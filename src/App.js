33fe63c991c1e1b5a4adda1c04ff3634690e1feb
duncan <dmckee95@gmail.com>
2023-02-13 13:23:52 -0500

    Add modal for loss or victory

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import DisplayLogo from "./Components/DisplayLogos";
+import EndGameModal from "./Components/EndGameModal";
 
 const App = () => {
   const [score, setScore] = useState(0);
+  const [tempScore, setTempScore] = useState(0)
   const [bestScore, setBestScore] = useState(0);
+  const [hidden, setHidden] = useState("background hidden")
 
   const increaseScore = () => {
     setScore(score + 1);
@@ -13,15 +16,28 @@ const App = () => {
   const logBestScore = () => {
     if (bestScore < score){
       setBestScore(score);
+      setTempScore(score)
       setScore(0)
     } else{
+      setTempScore(score)
       setScore(0)
     }
     ;
   };
 
+  const hiddenToggle = () =>{
+    if(hidden === "background hidden"){
+      setHidden("background")
+    } else{
+      setHidden("background hidden")
+    }
+  }
+
   return (
     <div className="App">
+      <div className={hidden}>
+        <EndGameModal tempScore={tempScore} bestScore = {bestScore} hiddenToggle = {hiddenToggle}/>
+      </div>
       <div className="header">
         <div className="title">
           <h1>NFL Memory Game!</h1>
@@ -36,6 +52,7 @@ const App = () => {
       <DisplayLogo
         increaseScore={() => increaseScore()}
         logBestScore={() => logBestScore()}
+        hiddenToggle = {hiddenToggle}
       />
       <div className="footer">
         <p>built by <a href="https://github.com/duncan-io">duncan-io</a></p>
